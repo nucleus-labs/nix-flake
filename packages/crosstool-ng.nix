fa62fa55f7b1df086cfe50ce77958880f07d5893
*Kim Zick (rummik) <k@9k1.us>
2023-07-29 22:04:41 -0400

    build crosstool-ng ourselves

--- packages/crosstool-ng.nix
+++ packages/crosstool-ng.nix
@@ -2,6 +2,8 @@
 , stdenv
 , fetchFromGitHub
 , makeWrapper
+, autoconf
+, automake
 
 , bc
 , binutils
@@ -28,17 +30,19 @@
 
 stdenv.mkDerivation {
   pname = "crosstool-ng";
-  version = "1.25.0-dev";
+  version = "1.25.0+git+465207b7";
 
   src = fetchFromGitHub {
     owner = "crosstool-ng";
     repo = "crosstool-ng";
     rev = "465207b7a21f00b94b934151c0667275d342cb56";
-    sha256 = "sha256-aBYvNCJDzUGJ7XxfTjuxMCyqPyy7+DMYeb0B/gbGDNM=";
+    sha256 = "sha256-bkw8w+oAGaHo3hwb3mWe3tpjrJ3HtT4rLhwXy5WpQcQ=";
   };
 
   nativeBuildInputs = [
     makeWrapper
+    autoconf
+    automake
 
     bc
     binutils
@@ -64,10 +68,19 @@ stdenv.mkDerivation {
     which
   ];
 
+  buildPhase = ''
+    bash ./bootstrap
+    ./configure --prefix=$out
+    make
+    make DESTDIR=$out install
+  '';
+
   postInstall = ''
     # add runtime dependencies to build toolchain
     wrapProgram $out/bin/ct-ng \
       --prefix PATH : ${lib.makeBinPath [
+          autoconf
+          automake
           bc
           binutils
           bison
