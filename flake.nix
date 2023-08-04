cf37211a324633d9d526e191240c106c6a927e44
*Kim Zick (rummik) <k@9k1.us>
2023-08-04 03:48:38 -0400

    flake: update platforms

--- flake.nix
+++ flake.nix
@@ -11,10 +11,9 @@
       ];
 
       systems = [
-        "aarch64-darwin"
+        "riscv64-linux"
         "aarch64-linux"
         "i686-linux"
-        "x86_64-darwin"
         "x86_64-linux"
       ];
 
